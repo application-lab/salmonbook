1cbb8c88f987a9085a2f8fd8eaedffbe05a6644a
Christian <christian@nyu.edu>
2019-10-08 15:07:48 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="profileCard">
+        <div className="profileImage" />
+        <div className="profileHeader">
+          <div>
+          <h1>Sam</h1>
+            <p>Columbia River, OR</p>
+          </div>
+          <div className="likes">
+            <h3>100 likes</h3>
+          </div>
+
+        </div>
+        <div className="profileDescription">
+          <p>Chinook are anadromous fish native to the North Pacific Ocean and the river systems of western North America, ranging from California to Alaska, as well as Asian rivers ranging from northern Japan to the Palyavaam River in the Arctic north-east Siberia.</p>
+        </div>
+        <div className="posts">
+          <img src="https://via.placeholder.com/50" alt="a river" />
+          <img src="https://via.placeholder.com/50" alt="a river" />
+          <img src="https://via.placeholder.com/50" alt="a river"/>
+          <img src="https://via.placeholder.com/50" alt="a river"/>
+          <img src="https://via.placeholder.com/50" alt="a river"/>
+        </div>
+        
+      </div>
+
     </div>
   );
 }
