b1ade0ddb0a2c226ce9c7c14a44ad1d37eae5050
Christian <christian@nyu.edu>
2019-10-22 12:22:34 +0800

    simplified and prep for map workshop

--- src/App.js
+++ src/App.js
@@ -9,7 +9,19 @@ let postImages = [
   'https://bit.ly/2OuzOyA'
 ];
 
+//here is our app function. we're passing a property to our <Profile /> component called postImage, with the value being
+// the first value of our postImages array (postImages[0]). Because we pass the same property a second time, but give it 
+// a different value, we'll see the second image from the postImages array on line 7
 
+
+/* 
+
+  OUR GOALS TODAY
+  1. we don't want to have to retype the <Profile /> component and the array value. We learned about the map() function
+     how could we used that make sure that no matter how many profiles we had, we'd be able to show each
+  2. we'd like to make our data model (the data we have related to posts) more robust. Let's think about content that might
+     make sense and add it. Now that we understand map() better, we can do lots of interesting things!
+*/
 function App() {
   return (
     <div>
@@ -29,18 +41,11 @@ function Profile(props) {
     <div className="profileCard">
       <div className="profileImage" />
       <div className="profileHeader">
-        <div>
           <h1>Sam</h1>
           <p>Columbia River, OR</p>
-        </div>
-        <div className="likes">
-          <h3>100 likes</h3>
-        </div>
-
       </div>
       <div className="profileDescription">
         <img src={props.postImage} alt="a lake" />
-        <p>{props.postContent}.</p>
       </div>
     </div>
   );
