51310a79fd3b7036a7b77b3f91c3a4b1125b1e02
Konrad Krawczyk <kk3144@NYUSH1824LP-MX.local>
2019-10-22 17:37:22 +0800

    deleted the vile heading + App.js is clean & unchanged

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ let postImages = [
 function App() {
   return (
     <div className="App">
-      <img className="headerImage" src="https://i.imgur.com/KwHPP8h.gif"></img>
       <Profile postImage={postImages[0]} postContent='chilling on this lake' />
       <Profile postImage={postImages[1]} postContent='hitting the gym' />
     </div>
